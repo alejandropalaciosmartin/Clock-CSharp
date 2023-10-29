b403298b39f5c00a6699470133def1e25fc0ac5e
Alejandro Palacios Martín <srjalean@gmail.com>
2023-10-29 18:10:36 +0100

    Limpieza de codigo

--- Reloj/Reloj/Program.cs
+++ Reloj/Reloj/Program.cs
@@ -1,36 +1,32 @@
-﻿namespace Reloj
+﻿class Proram
 {
-    class Proram
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.SetCursorPosition(15, 2);
+        Console.WriteLine("Mi reloj digital");
+
+        int hours, minutes, seconds;
+        DateTime now = DateTime.Now;
+
+        hours = now.Hour;
+        minutes = now.Minute;
+        seconds = now.Second;
+
+        while (true)
         {
-            Console.SetCursorPosition(15, 2);
-            Console.WriteLine("Mi reloj digital");
-
-            int hours, minutes, seconds;
-            DateTime now = DateTime.Now;
-
-            hours = now.Hour;
-            minutes = now.Minute;
-            seconds = now.Second;
-
-            while (true)//El "while es para que cuando llegue a 24 vuelva a 00
-            {
-                for (; minutes < 60; minutes++)
+            for(; minutes < 60; minutes++)
+            { 
+                for(; seconds < 60; seconds++)
                 {
-                    for (; seconds < 60; seconds++)
-                    {
-                        Console.SetCursorPosition(20, 10);
-                        Console.WriteLine($"{hours:00}:{minutes:00}:{seconds:00}");
-                        Thread.Sleep(1000);
-                    }
-                    seconds = 0;
+                    Console.SetCursorPosition(20, 10);
+                    Console.WriteLine($"{hours:00}:{minutes:00}:{seconds:00}");
+                    Thread.Sleep(1000);
                 }
-                minutes = 0;
-
-                hours++;
-                if (hours >= 24) hours = 0;
+                seconds = 0;
             }
+            minutes = 0;
+            hours++;
+            if (hours >= 24) hours = 0;
         }
     }
 }
