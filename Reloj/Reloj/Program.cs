b1757b9c18b6105a1e670a69ec4fded841d4fc7c
Alejandro Palacios Martín <srjalean@gmail.com>
2023-10-29 18:13:33 +0100

    Código reducido

--- Reloj/Reloj/Program.cs
+++ Reloj/Reloj/Program.cs
@@ -1,32 +1,16 @@
-﻿class Proram
+﻿class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
         Console.SetCursorPosition(15, 2);
         Console.WriteLine("Mi reloj digital");
 
-        int hours, minutes, seconds;
-        DateTime now = DateTime.Now;
-
-        hours = now.Hour;
-        minutes = now.Minute;
-        seconds = now.Second;
-
         while (true)
         {
-            for(; minutes < 60; minutes++)
-            { 
-                for(; seconds < 60; seconds++)
-                {
-                    Console.SetCursorPosition(20, 10);
-                    Console.WriteLine($"{hours:00}:{minutes:00}:{seconds:00}");
-                    Thread.Sleep(1000);
-                }
-                seconds = 0;
-            }
-            minutes = 0;
-            hours++;
-            if (hours >= 24) hours = 0;
+            DateTime now = DateTime.Now;
+            Console.SetCursorPosition(20, 10);
+            Console.WriteLine($"{now:HH:mm:ss}");
+            System.Threading.Thread.Sleep(1000);
         }
     }
 }
